c35e66dab0dd50c29c3b4224aa4bf25ab5454d11
ryan-reshadi <ryan@reshadi.com>
2026-09-10 22:21:14 -0700

    Added the Bridge Network to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -122,6 +122,9 @@ export default function Footer() {
               <span style={{ color: 'rgba(255,255,255,0.6)', fontSize: 14 }}>
                 PinkAware
               </span>
+              <span style={{ color: 'rgba(255,255,255,0.6)', fontSize: 14 }}>
+                The Bridge Network
+              </span>
             </div>
           </div>
 
